5ef7b5595ec506ed423c55aa6d1afe66ce50be49
meatnordrink <runawaytrike@gmail.com>
2020-03-14 12:44:53 -0400

    "options clear working; trying to add score (not working)

--- phq9/src/App.js
+++ phq9/src/App.js
@@ -9,34 +9,42 @@ import './App.css';
 // To-do, code:
 // Refactor to set appropriate CSS elements as a theme; figure out CSS-in-JSS. (In general, see if I'm including Material-ui specifiers in the most elegant way.)
 // Refactor MakeQuestion, RenderOptions to accepts props of array with questions, options.
+// Deal with what happens when we get to end of questions array.
+// Record answers as variables; display at end.
 
-/// SEE ????: NEED TO FIGURE OUT HOW TO MAKE AN EVENT SUCH THAT  `this.questionRenderer` fires again on button click. 
 
 class MakeQuestion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      questionNumber: true,
+      questionNumber: 0,
+      userScore: 0
     };
     this.updateQuestionNumber = this.updateQuestionNumber.bind(this);
     this.questionRenderer = this.questionRenderer.bind(this);
   }
 
+  questions = ["Little interest or pleasure in doing things.", "Feeling down, depressed, or hopeless.", "Trouble falling or staying asleep, or sleeping too much.", "Feeling tired or having little energy.", "Poor appetite or overeating.", "Feeling bad about yourself - or that you are a failure or have let yourself or your family down.", "Over the last 2 weeks, how often have you had trouble concentrating on things?", "Moving or speaking so slowly that other people could've noticed? Or the opposite - being much more fidgety or restless than usual.", "Thoughts about just wanting to fall asleep and not wake up, harming yourself, or killing yourself?"]
+  // eventually, write a function to bold "2 weeks"
+
   questionRenderer() {
     var questionNumber = this.state.questionNumber;
+    var questions = this.questions;
     return (
       <RenderQuestions 
         questionNumber={questionNumber}
+        questions={questions}
       />
     )
 
   }
 
-  updateQuestionNumber() {
+  updateQuestionNumber(score) {
     var questionNumber = this.state.questionNumber;
     questionNumber++;
     this.setState({
-      questionNumber: questionNumber
+      questionNumber: questionNumber,
+      userScore: userScore + score
     })
   }
 
@@ -66,6 +74,9 @@ class MakeQuestion extends React.Component {
               </Grid>
               <RenderOptions
                 updateQuestionNumber={this.updateQuestionNumber}
+                questionNumber={this.state.questionNumber}
+                questions={this.questions}
+                userScore={this.state.userScore}
                  />
         </Grid>
       </Paper>
@@ -73,7 +84,6 @@ class MakeQuestion extends React.Component {
   }
 }
 function RenderQuestions(props) {
-  let questions = ["Little interest or pleasure in doing things.", "Feeling down, depressed, or hopeless.", "Trouble falling or staying asleep, or sleeping too much.", "Feeling tired or having little energy.", "Poor appetite or overeating.", "Feeling bad about yourself - or that you are a failure or have let yourself or your family down.", "Over the last *2 weeks*, how often have you had trouble concentrating on things?", "Moving or speaking so slowly that other people could've noticed? Or the opposite - being much more fidgety or restless than usual.", "Thoughts about just wanting to fall asleep and not wake up, harming yourself, or killing yourself?"]
   return(
   <Typography 
     variant="h6" 
@@ -81,11 +91,18 @@ function RenderQuestions(props) {
     align="center"
     style={{width:360}}
     >
-      {questions[props.questionNumber]} 
+      {props.questions[props.questionNumber]} 
   </Typography>
   )
  }
 function RenderOptions(props) {
+    if (props.questionNumber >= props.questions.length) {
+      return(
+        <Typography>
+          Your depression score is: {props.userScore}
+        </Typography>
+      );
+    }
     let options = ["Not at all", "Several Days", "More than half the days", "Nearly every day"];
     let buttons = [];
     for (let i=0; i < options.length; i++) {
@@ -97,7 +114,8 @@ function RenderOptions(props) {
             color="primary"
             // fullWidth="true"
             style={{width:200}}
-            onClick={props.updateQuestionNumber}
+            score={i+1}
+            onClick={props.updateQuestionNumber(score)}
             > 
             {options[i]} 
           </Button>
@@ -107,6 +125,7 @@ function RenderOptions(props) {
     return (buttons);
 }
 
+// ultimately, I want to be passingin the questions and options arrays as props on the MakeQuestion calls below. 
 function App() {
   return (
     <MakeQuestion />
