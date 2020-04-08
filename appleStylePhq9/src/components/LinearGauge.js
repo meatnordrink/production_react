3c8e0af698431530ab060d138b3f9bd9c5b84e1c
meatnordrink <andy@uplift.us>
2020-04-08 12:21:33 -0400

    closer...

--- appleStylePhq9/src/components/LinearGauge.js
+++ appleStylePhq9/src/components/LinearGauge.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Grid, Typography, Card, Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
+import { Grid, Typography, Card, Radio, RadioGroup, FormControlLabel, FormHelperText } from '@material-ui/core';
 import { ThemeProvider, createMuiTheme, useTheme, makeStyles, styled } from '@material-ui/core/styles'
 
 var userScore = '-210px';
@@ -10,33 +10,38 @@ const otherStyles = makeStyles({
         transition: '4s'
     },
     choiceCards: {
-        // display: 'flex',
+        display: 'flex',
         background: 'linear-gradient(90deg, red, orange, yellow, 80%, #04ef04)',
         transition: '4s',
         width: 480,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
         // fontSize: 18,
         // fontWeight: 700,
         boxShadow: '0 1px 3px rgba(0,0,0,.1)',
         borderRadius: 11
     },
     choiceText: {
-        fontSize: 18,
-        fontWeight: 700
+        display: 'flex', 
+        color: '#748afb',
+        fontSize: 14,
+        fontWeight: 400
     },
     radios: {
         // textAlign: 'right',
         display: 'flex',
-        justifyContent: 'space-between',
-        padding: '8px 10px',
-        marginLeft: 8,
-        // marginRight: 2,
+        justifyContent: 'flex-end',
+        padding: '8px 0px 0px 0px',
+        marginLeft: 0,
     },
     circle: {
-        color: '#ededed'
+        color: '#ededed',
+        paddingTop: 0
     },
     changed: {
         color: '#748afb',
         transition: '4s',
+        paddingTop: 0,
         transform: props => props.transform
     },
 })
@@ -61,7 +66,7 @@ export default function(props) {
         >
         <Card className={classes.choiceCards}>
             <RadioGroup color="primary" className={classes.choiceCards} value='disabled'>
-                <FormControlLabel className={classes.radios} control={<Radio color="primary" className={radioStyle}/>} label={<Typography className={classes.choiceText}></Typography>} labelPlacement="start" />
+                <FormControlLabel className={classes.radios} control={<Radio color="primary" className={radioStyle}/>} label={<Typography className={classes.choiceText}>Your Score</Typography>} labelPlacement="top" />
             </RadioGroup>
         </Card>
     </Grid>
