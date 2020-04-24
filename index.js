859e29b419661315833246a1096ff08134ac15ad
meatnordrink <andy@uplift.us>
2020-04-24 11:45:35 -0400

    adding dotfiles to index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let express = require("express")
 let path = require("path");
 app = express()
-app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build")))
+app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build"), {
+    dotfiles: "allow"
+}))
 app.listen(80)
