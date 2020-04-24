fa9c811da8bb19fa19c6f535e0e0325aef068c13
meatnordrink <andy@uplift.us>
2020-04-24 12:06:50 -0400

    ""

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ app = express()
 app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build"), {
     dotfiles: "allow"
 }))
+app.use(express.static(path.join(__dirname, "appleBUild", "build")));
+
 app.listen(80)
