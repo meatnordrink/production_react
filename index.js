8c2f8e358efcb6593fcdd1f6819df5ac5e9ce73a
meatnordrink <andy@uplift.us>
2020-04-24 12:14:46 -0400

    ""

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let express = require("express")
 let path = require("path");
 app = express()
-app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build", ".well-known"), {
+app.use("/.well-known", express.static(path.join(__dirname, ".well-known"), {
     dotfiles: "allow"
 }))
-app.use(express.static(path.join(__dirname, "appleBUild", "build")));
+app.use(express.static(path.join(__dirname, "text.txt")));
 
 app.listen(80)
