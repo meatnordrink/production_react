e32a7141c8f43bb9c3bf133996fe22bdb4849233
meatnordrink <andy@uplift.us>
2020-04-24 12:53:36 -0400

    ''

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 let express = require("express")
-let serveStatic = require("serve-static");
-app = express()
-
-app.use("/.well-known", serveStatic("./.well-known/", {
-	dotfiles: "allow",
-}));
-
-app.use(serveStatic("./text.txt"));
-
+let serveStatic = require("serve-static")
+let app = express()
+app.use(serveStatic("public", {"dotfiles": "allow"}))
 app.listen(80)
