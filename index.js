f86f909f19c3e7c81f694a0d4524f3074516c7cb
meatnordrink <andy@uplift.us>
2020-04-24 12:11:17 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express")
 let path = require("path");
 app = express()
-app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build"), {
+app.use("/.well-known", express.static(path.join(__dirname, "appleBuild", "build", ".well-known"), {
     dotfiles: "allow"
 }))
 app.use(express.static(path.join(__dirname, "appleBUild", "build")));
